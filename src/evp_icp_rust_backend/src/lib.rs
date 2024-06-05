f2415294aeaf9fe5968220ff17de18a6c759bfc0
Stephen-Kimoi <moenisteve@gmail.com>
2024-06-05 09:15:41 +0300

    getting ecdsa public key

--- src/evp_icp_rust_backend/src/lib.rs
+++ src/evp_icp_rust_backend/src/lib.rs
@@ -1,4 +1,23 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use ic_cdk::update; 
+use ic_cdk::api::management_canister::ecdsa::{ecdsa_public_key, EcdsaCurve, EcdsaKeyId, EcdsaPublicKeyArgument, EcdsaPublicKeyResponse}; 
+
+#[update]
+async fn get_ecdsa_public_key() -> EcdsaPublicKeyResponse {
+    let (pub_key,) = ecdsa_public_key(EcdsaPublicKeyArgument {
+        key_id: key_id(), 
+        ..Default::default()
+    })
+    .await
+    .expect("Failed to get ECDSA public key");
+    
+    pub_key
 }
+
+fn key_id() -> EcdsaKeyId {
+    EcdsaKeyId {
+        curve: EcdsaCurve::Secp256k1, 
+        name: "dfx_test_key".to_string() 
+    }
+}
+
+ic_cdk::export_candid!(); 
